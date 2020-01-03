06d16d167f659416ca08e879b72d461c3fb9a939
starbucksdolcelatte <seoyoon9535@gmail.com>
2020-01-04 02:06:15 +0900

    009 - 모두의 약수

--- 009_factors.cpp
+++ 009_factors.cpp
@@ -1,6 +1,30 @@
 #include <stdio.h>
+#include <iostream>
 using namespace std;
 
 int main(){
+	int n, i, j, idx;
+	
+	//input
+	//freopen("input.txt", "rt", stdin);
+	scanf("%d", &n);
+	int* arr = new int[n+1]();
+	
+	for(i=1; i<n+1; i++){
+		j=i;
+		while(j<n+1){
+			arr[j]++;
+			j += i;
+		}
+		/*
+		for(j=1; j<n+1; j++){
+			printf("%d ", arr[j]);
+		}
+		printf("\n");
+		*/
+	}
+	for(i=1; i<n+1; i++){
+		printf("%d ", arr[i]);
+	}
 	return 0;
 }
